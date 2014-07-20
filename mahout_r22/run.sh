24fbb60c90513469773d95e662aa3e59afd58b5d
Lukas Elmer <lukas.elmer@gmail.com>
2014-07-20 03:02:50 +0200

    run 22

--- mahout_r22/run.sh
+++ mahout_r22/run.sh
@@ -4,7 +4,7 @@
 # and http://www.slideshare.net/Hadoop_Summit/stella-june27-1150amroom210av2
 # and http://stackoverflow.com/questions/21318459/how-to-run-mahout-cvb-on-reuters-news-on-cloudera-vm-cdh4-5-as-lda-is-not-longer
 # and http://qnalist.com/questions/4878421/extracting-document-topic-inference-with-the-new-lda-cvb-algorithm
-RUN="21"
+RUN="22"
 MAHOUT="mahout"
 HDFS_DIR="/runs/r${RUN}"
 WORK_DIR="`pwd`/out"
