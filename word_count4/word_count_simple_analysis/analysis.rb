1d058c690a7fbf5ddb16dfa435b886739ecb2575
Lukas Elmer <lukas.elmer@gmail.com>
2014-07-23 00:26:26 +0200

    word count analysis

--- word_count4/word_count_simple_analysis/analysis.rb
+++ word_count4/word_count_simple_analysis/analysis.rb
@@ -1,16 +1,26 @@
 #! /usr/bin/ruby
 
-def command(offset, num_words=16)
+def print_extract(offset, num_words=16)
   puts "-- Words #{offset-15}-#{offset+1}"
   puts `head -n+#{offset} word_counts.txt | tail -n#{num_words}`
   puts
 end
 
-index = 16
+def calc_offset(index)
+  # used: 1.25, 1.5, 2, Math.E
+  (2 ** index).to_i
+end
 
-while(index < 43740086) do
-  command(index)
-  index = index * 2
+index = 1
+
+while(true) do
+  offset = calc_offset(index)
+  index += 1
+
+  # 43740086 is the number of lines in word_counts.txt
+  break if offset >= 43740086
+
+  print_extract(offset)
 end
 
 
