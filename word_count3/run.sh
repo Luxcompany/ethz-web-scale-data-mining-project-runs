448c664bbd613ca0b438cafafb30500736bd6fe4
Lukas Elmer <lukas.elmer@gmail.com>
2014-07-22 23:04:24 +0200

    word count scripts and logs

--- word_count3/run.sh
+++ word_count3/run.sh
@@ -4,7 +4,7 @@
 # and http://www.slideshare.net/Hadoop_Summit/stella-june27-1150amroom210av2
 # and http://stackoverflow.com/questions/21318459/how-to-run-mahout-cvb-on-reuters-news-on-cloudera-vm-cdh4-5-as-lda-is-not-longer
 # and http://qnalist.com/questions/4878421/extracting-document-topic-inference-with-the-new-lda-cvb-algorithm
-RUN="1"
+RUN="3"
 HDFS_DIR="/runs/r${RUN}"
 WORK_DIR="`pwd`"
 OUT_DIR="/disk3/user_work/runs/word_count${RUN}"
