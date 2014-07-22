59a2dbbf2f59a8ae3fe4844d4a34eb9a6abf7da2
Lukas Elmer <lukas.elmer@gmail.com>
2014-07-23 00:36:31 +0200

    analysis

--- word_count4/word_count_simple_analysis/analysis.rb
+++ word_count4/word_count_simple_analysis/analysis.rb
@@ -1,8 +1,13 @@
 #! /usr/bin/ruby
 
-def print_extract(offset, num_words=16)
-  puts "-- Words #{offset-15}-#{offset+1}"
-  puts `head -n+#{offset} word_counts.txt | tail -n#{num_words}`
+# small: num_words=16
+# large: num_words=150
+def print_extract(offset, num_words=150)
+  # very cheap workaround for corner case...
+  offset = 0 if offset < num_words
+
+  puts "-- Words #{offset + 1}-#{offset + num_words + 1}"
+  puts `head -n+#{offset + num_words} word_counts.txt | tail -n#{num_words}`
   puts
 end
 
@@ -11,16 +16,16 @@ def calc_offset(index)
   (2 ** index).to_i
 end
 
-index = 1
+index = 7
 
 while(true) do
   offset = calc_offset(index)
-  index += 1
 
   # 43740086 is the number of lines in word_counts.txt
   break if offset >= 43740086
 
   print_extract(offset)
+  index += 1
 end
 
 
