4cd5ab6133f739f87b75bfe3e21a6ef9e4ba4ee0
Lukas Elmer <lukas.elmer@gmail.com>
2014-07-22 23:21:11 +0200

    word count 4

---
+++ word_count4/readme.md
@@ -0,0 +1,7 @@
+# Simple word count on the cleaned dataset
+
+Version: https://github.com/lukaselmer/ethz-web-scale-data-mining-project/tree/947551709e1833590dc7d1ddb830bca28d99b2db/src/word_count
+
+Try #4
+
+
---
+++ word_count4/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Following: http://theglassicon.com/computing/machine-learning/running-lda-algorithm-mahout
+# and http://www.slideshare.net/Hadoop_Summit/stella-june27-1150amroom210av2
+# and http://stackoverflow.com/questions/21318459/how-to-run-mahout-cvb-on-reuters-news-on-cloudera-vm-cdh4-5-as-lda-is-not-longer
+# and http://qnalist.com/questions/4878421/extracting-document-topic-inference-with-the-new-lda-cvb-algorithm
+RUN="4"
+HDFS_DIR="/runs/r${RUN}"
+WORK_DIR="`pwd`"
+OUT_DIR="/disk3/user_work/runs/word_count${RUN}"
+
+/root/spark/bin/spark-submit --class WordCountApp ${OUT_DIR}/run.jar >> ${OUT_DIR}/log.log 2>&1
+
+
