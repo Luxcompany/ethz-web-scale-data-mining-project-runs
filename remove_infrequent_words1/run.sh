c21a9214725d26aa96a40429595f7c26c2970a55
Lukas Elmer <lukas.elmer@gmail.com>
2014-07-23 04:20:38 +0200

    remove infrequent words

--- remove_infrequent_words1/run.sh
+++ remove_infrequent_words1/run.sh
@@ -5,10 +5,8 @@
 # and http://stackoverflow.com/questions/21318459/how-to-run-mahout-cvb-on-reuters-news-on-cloudera-vm-cdh4-5-as-lda-is-not-longer
 # and http://qnalist.com/questions/4878421/extracting-document-topic-inference-with-the-new-lda-cvb-algorithm
 RUN="1"
-HDFS_DIR="/runs/r${RUN}"
-WORK_DIR="`pwd`"
-OUT_DIR="/disk3/user_work/runs/word_count${RUN}"
+OUT_DIR="/disk3/user_work/runs/remove_infrequent_words${RUN}"
 
-/root/spark/bin/spark-submit --class WordCountApp ${WORK_DIR}/${RUN}/run.jar >> ${WORK_DIR}/${RUN}/log.log 2>&1
+/root/spark/bin/spark-submit --class RemoveInfrequentWordsApp ${OUT_DIR}/run.jar >> ${OUT_DIR}/log.log 2>&1
 
 
