b4015781e6b2f132e82e51055d1754136da22d13
Lukas Elmer <lukas.elmer@gmail.com>
2014-07-23 06:02:55 +0200

    remove infrequent words

--- remove_infrequent_words11/run.sh
+++ remove_infrequent_words11/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MIN_WORD_COUNT=500
+MIN_WORD_COUNT=1000
 OUT_DIR=`pwd`
 
 MIN_WORD_COUNT="${MIN_WORD_COUNT}" OUTPUT="hdfs://dco-node121.dco.ethz.ch:54310/cw-combined-pruned-${MIN_WORD_COUNT}" /root/spark/bin/spark-submit --total-executor-cores 20 --class RemoveInfrequentWordsApp ${OUT_DIR}/run.jar > ${OUT_DIR}/log.log 2>&1
