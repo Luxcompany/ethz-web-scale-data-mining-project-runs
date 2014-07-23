324a5444d45ae8fc1222c675b14820e53ea13244
Lukas Elmer <lukas.elmer@gmail.com>
2014-07-23 23:05:50 +0200

    git config scripts

---
+++ set_git_config_dco121.sh
@@ -0,0 +1,6 @@
+git config user.email "lukas.elmer+doc-node121@gmail.com"
+git config user.name "doc-node121"
+
+git config --global user.email "lukas.elmer+doc-node121@gmail.com"
+git config --global user.name "doc-node121"
+
---
+++ set_git_config_lukas.sh
@@ -0,0 +1,6 @@
+git config user.name "Lukas Elmer"
+git config user.email "lukas.elmer@gmail.com"
+
+git config --global user.name "Lukas Elmer"
+git config --global user.email "lukas.elmer@gmail.com"
+
