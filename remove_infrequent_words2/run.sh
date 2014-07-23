299ace59d11f240e25fc8120a9da169848d3a147
Lukas Elmer <lukas.elmer@gmail.com>
2014-07-23 04:58:06 +0200

    run 2

---
+++ remove_infrequent_words2/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+RUN="2"
+OUT_DIR="/disk3/user_work/runs/remove_infrequent_words${RUN}"
+
+OUTPUT="hdfs://dco-node121.dco.ethz.ch:54310/cw-combined-pruned-5000" MIN_WORD_COUNT=5000 /root/spark/bin/spark-submit --class RemoveInfrequentWordsApp ${OUT_DIR}/run.jar >> ${OUT_DIR}/log.log 2>&1
+
+
