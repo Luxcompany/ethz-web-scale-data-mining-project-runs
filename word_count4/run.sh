52fc357e20c4893ca9419f4dc40b941eb493b10e
Lukas Elmer <lukas.elmer@gmail.com>
2014-07-23 23:59:37 +0200

    Update run.sh

--- word_count4/run.sh
+++ word_count4/run.sh
@@ -5,8 +5,6 @@
 # and http://stackoverflow.com/questions/21318459/how-to-run-mahout-cvb-on-reuters-news-on-cloudera-vm-cdh4-5-as-lda-is-not-longer
 # and http://qnalist.com/questions/4878421/extracting-document-topic-inference-with-the-new-lda-cvb-algorithm
 RUN="4"
-HDFS_DIR="/runs/r${RUN}"
-WORK_DIR="`pwd`"
 OUT_DIR="/disk3/user_work/runs/word_count${RUN}"
 
 /root/spark/bin/spark-submit --class WordCountApp ${OUT_DIR}/run.jar >> ${OUT_DIR}/log.log 2>&1
